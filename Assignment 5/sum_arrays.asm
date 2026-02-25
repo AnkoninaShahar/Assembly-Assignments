8652e0ccc9fa6efbb96f0c497e137a3362313c3c
Shahar Ankonina <shahar.ankonina05@gmail.com>
2026-02-25 15:01:38 -0800

    Cleaned up sum_arrays

--- Assignment 5/sum_arrays.asm
+++ Assignment 5/sum_arrays.asm
@@ -2,7 +2,7 @@ COMMENT !
 	Description:	
 
 	Author:         SHAHAR ANKONINA
-	Date:           02/25/2026	LAST EDITED: 
+	Date:           03/11/2026	LAST EDITED: 02/25/2026
 !
 
 INCLUDE Irvine32.inc	; Includes the Irvine32.inc library
@@ -18,40 +18,46 @@ upperLimit dword 40
 array_1 sdword 10,30,25,15,17,19,40,41,43    ; test case 1
 array_2 sdword 10,-30,25,15,-17,55,40,41,43  ; test case 2
 
+SumArray proto, 
+	array:DWORD,
+	len:DWORD,
+	index:DWORD
+
 .code
-SumArray proc
-	mov eax, 0
-	mov ecx, edi
-	mov esi, 0
-
-	sum_elements:
-		mov ebx, [edx + esi]
-		add esi, 4
-
-		cmp ebx, lowerLimit
-		jl continue
-
-		cmp ebx, upperLimit
-		jg continue
-
-		add eax, ebx
-
-		continue:
-			loop sum_elements
-	ret
-SumArray endp
-
 main proc
-	mov edx, OFFSET array_1
-	mov edi, LENGTHOF array_1
-	call SumArray
+	invoke SumArray, OFFSET array_1, LENGTHOF array_1, TYPE array_1
 	call DumpRegs
 
-	mov edx, OFFSET array_2
-	mov edi, LENGTHOF array_2
-	call SumArray
+	call SumArray, OFFSET array_2, LENGTHOF array_2, TYPE array_2
 	call DumpRegs
 
 	invoke ExitProcess,0
 main endp
+
+SumArray proc uses ebx ecx edx esi,
+	array:DWORD,
+	len:DWORD,
+	index:DWORD
+	
+	mov eax, 0
+	mov edx, array
+	mov ecx, len
+	mov esi, 0
+
+	sum_elements:
+		mov ebx, [edx + esi]
+		add esi, index
+
+		cmp ebx, lowerLimit
+		jl skip_addition
+
+		cmp ebx, upperLimit
+		jg skip_addition
+
+		add eax, ebx
+
+		skip_addition:
+			loop sum_elements
+	ret
+SumArray endp
 end main
