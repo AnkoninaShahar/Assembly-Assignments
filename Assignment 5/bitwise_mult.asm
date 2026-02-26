bbf620239d61a0f5a30e7a8a2edb958ba06e5ac1
Shahar Ankonina <shahar.ankonina05@gmail.com>
2026-02-25 21:45:12 -0800

    Completed Assignment 5

--- Assignment 5/bitwise_mult.asm
+++ Assignment 5/bitwise_mult.asm
@@ -1,8 +1,26 @@
 COMMENT !
-	Description:	
+	Description:	This program multiplies two integers together using only "bitwise multiplication".
+					Shifting an integer to the left multiplies it by 2^[the amount shifted].
+					Using the formula:		
+									
+									multiplicand * 2^[bit degree of the multiplier]
+									(in other words shifting a multiplicand to the left
+									by the amount of time equivelant to the degree of a 
+									bit within a multiplier)
+
+					and adding the sums together for each bit of the multiplier, the computer
+					is able to multiply integers in the most efficient way possible. By the end of the
+					process, the product is stored in the EAX register.
+
+					This lab relates to the lessons learned in chapter 7 of the textbook as this assignment 
+					requires the use of bitwise shifting and knowledge on its affect on a integer.
+					It further relates to lessons learned in chapter 5 and 8 of the textbook as writing a 
+					procedure is also a requirement for this assigment.
+					It also relates to lessons learned in chapter 6 of the textbook through the necessity 
+					of conditional jumps used within the loop to determine whether a bit is 1 or 0.
 
 	Author:         SHAHAR ANKONINA
-	Date:           02/25/2026	LAST EDITED: 
+	Date:           03/11/2026	LAST EDITED: 02/25/2026
 !
 
 INCLUDE Irvine32.inc	; Includes the Irvine32.inc library
@@ -12,53 +30,93 @@ INCLUDE Irvine32.inc	; Includes the Irvine32.inc library
 .stack 4096
 ExitProcess proto,dwExitCode:dword
 
+MULTIPLIER_BIT_SIZE EQU 32						; Size of the multiplier in bits
+
 .data
-multiplicands DWORD 65531, 699050, 21
-multipliers DWORD 1029, 5461, 178956970
+; Given test data
+multiplicand DWORD 65531, 699050, 21
+multiplier DWORD 1029, 5461, 178956970
 
 .code
+; ------------------------------------------------------------------
+;	BitwiseMultiply
+;	Multiplies two DWORD integers together using bitwise operations
+;	Receives: EAX: multiplier
+;			  EBX: multiplicand
+;	Returns: EAX: calculated product
+; ------------------------------------------------------------------
 BitwiseMultiply proc
-	LOCAL count: BYTE
-	mov count, 0
-	mov ebx, eax
-	mov eax, 0
-	mov cl, 0
-	mov ecx, 32
+	LOCAL shift_count: BYTE						; Local variable to hold the amount to shift by depending on the degree of the bit
+
+	; Pushing all the used registers onto the stack
+	;--------------------------------------------------------
+	push ecx									; ECX: the count for the number of loops (MULTIPLIER_BIT_SIZE) 						
+	push edx									; EDX: stores the value of the multiplier
+
+	; Presets registers and variables for the loop
+	;--------------------------------------------------------
+	mov edx, eax								; Stores the multiplier value in the EDX register
+	mov eax, 0									; Resets the EAX register
+	mov ecx, MULTIPLIER_BIT_SIZE				; Sets the loop count to the bit size of the multiplier
+	mov shift_count, 0							; Initializes the shift_count local variable to 0
+
+	; Loop that calculates the product of the integers
+	;--------------------------------------------------------
 	multiply:
-		shr ebx, 1
-		jnc continue
+		; Early exit test: 
+		; If the multiplier is small, the loop won't waste time looping over the extra zeros
+		cmp edx, 0								; Checks if EDX is zero
+		jz exit_loop							; If so -> exits loop
 
-		push ecx
-		
-		push edx
+		shr edx, 1								; Shifts the bits of the multiplier to the right by 1
+		jnc skip_multiplication					; If a 0 is shifted into the Carry Flag -> skips the multiplication
 
-		mov edx, edi
-		mov cl, count
-		shl edx, cl
-		add eax, edx
+		; Pushes all the used registers in this section onto the stack
+		push ecx								; CL: stores the amount to shift by
+		push ebx								; EBX: makes copy in stack so original multiplicand value is not affected
 
-		pop edx
+		; Bitwise shifting operations to multiply values
+		mov cl, shift_count						; Stores the amount to shift by in the CL register
+		shl ebx, cl								; Shifts the value in the EBX register by the value in the CL register
+		add eax, ebx							; Adds the value in the EBX register to the total product in the EAX register
+
+		; Returns all the used registers in this section from the stack
+		pop ebx	
 		pop ecx
-		continue:
-			inc count
+
+		skip_multiplication:					; Code label to skip multiplication process
+			inc shift_count						; Increments the amount to shift by to match the degree of the current bit
+			; Decrements ECX register (loop counter)
+			; Checks if the ECX register is 0
+			; If so -> exit loop, else -> loop multiply
 			loop multiply
 
-	ret
+	; Returns all the used registers from the stack
+	;--------------------------------------------------------
+	exit_loop:
+		pop edx
+		pop ecx
+
+	ret											; Returns back to the call statment
 BitwiseMultiply endp
 
 main proc
-	mov ecx, LENGTHOF multiplicands
-	mov esi, 0
-	multiplication:
-		push ecx
-		mov eax, multipliers[esi]
-		mov edi, multiplicands[esi]
-		call BitwiseMultiply
-		call DumpRegs
+	; Presets registers for the loop
+	mov ecx, LENGTHOF multiplicand				; Sets the loop count to the length of multiplicand
+	mov esi, 0									; Sets the starting index to 0
 
-		add esi, 4
-		pop ecx
-		loop multiplication
+	; Loop that runs the multiply procedure over every pair of multiplicand and multiplier 
+	main_loop:
+		mov eax, multiplier[esi]				; Stores the value of the multiplier in the EAX register
+		mov ebx, multiplicand[esi]				; Stores the value of the multiplicand in the EBX register
+		call BitwiseMultiply					; Calls the BitwiseMultiply procedure using the values stored in the EAX and EBX registers
+		call DumpRegs							; Displays every register
+
+		add esi, TYPE multiplicand				; Increments the index to the next element
+		; Decrements ECX register (loop counter)
+		; Checks if the ECX register is 0
+		; If so -> exit loop, else -> loop main_loop
+		loop main_loop
 
 	invoke ExitProcess,0
 main endp
--- Assignment 5/sum_arrays.asm
+++ Assignment 5/sum_arrays.asm
@@ -1,5 +1,13 @@
 COMMENT !
-	Description:	
+	Description:	This program adds up all the values in an array within a given limit (in this case
+					20 <= x <= 40). It does this by looping through the given array, checking each element, 
+					and adding it to the total sum (EAX register) if the integer falls within the limits 
+					provided.
+
+					This lab relates to lessons learned in chapter 6 of the textbook when using conditional 
+					jumps to determine if an integer falls within the limit alongside the compare instruction.
+					It further relates to lessons learned in chapter 5 and 8 of the textbook as writing a 
+					procedure is handy in keeping the code clean, readable, and reusable.
 
 	Author:         SHAHAR ANKONINA
 	Date:           03/11/2026	LAST EDITED: 02/25/2026
@@ -13,51 +21,71 @@ INCLUDE Irvine32.inc	; Includes the Irvine32.inc library
 ExitProcess proto,dwExitCode:dword
 
 .data
-lowerLimit dword 20
-upperLimit dword 40
-array_1 sdword 10,30,25,15,17,19,40,41,43    ; test case 1
-array_2 sdword 10,-30,25,15,-17,55,40,41,43  ; test case 2
+; Given test data
+lowerLimit DWORD 20
+upperLimit DWORD 40
+array_1 SDWORD 10,30,25,15,17,19,40,41,43		; test case 1
+array_2 SDWORD 10,-30,25,15,-17,55,40,41,43		; test case 2
 
+; Sums up elements within a given array
 SumArray proto, 
-	array:DWORD,
-	len:DWORD,
-	index:DWORD
+	array_offset: DWORD,						; The offset address of the array
+	array_length: DWORD,						; The number of elements in the array
+	element_size: DWORD							; The size of each element within the array
 
 .code
 main proc
+	; Calls the SumArray procedure with the given arguments for array_1
 	invoke SumArray, OFFSET array_1, LENGTHOF array_1, TYPE array_1
-	call DumpRegs
+	call DumpRegs								; Displays every register
 
-	call SumArray, OFFSET array_2, LENGTHOF array_2, TYPE array_2
-	call DumpRegs
+	; Calls the SumArray procedure with the given arguments for array_2
+	invoke SumArray, OFFSET array_2, LENGTHOF array_2, TYPE array_2
+	call DumpRegs								; Displays every register
 
 	invoke ExitProcess,0
 main endp
 
+; ------------------------------------------------------------------
+;	SumArray
+;	Calculates the sum of a given SDWORD array within a given range.
+;	Receives: array_offset (DWORD): The starting address of the array
+;			  array_length (DWORD): The number of elements to process
+;			  element_size (DWORD): The byte size of each element
+;	Returns: EAX: calculated sum within [lowerLimit, upperLimit]
+; ------------------------------------------------------------------
 SumArray proc uses ebx ecx edx esi,
-	array:DWORD,
-	len:DWORD,
-	index:DWORD
-	
-	mov eax, 0
-	mov edx, array
-	mov ecx, len
-	mov esi, 0
+	array_offset: DWORD,						; The offset address of the array
+	array_length: DWORD,						; The number of elements in the array
+	element_size: DWORD							; The size of each element within the array
 
+	; Presets registers for the loop
+	;--------------------------------------------------------
+	mov eax, 0									; Resets the EAX register
+	mov edx, array_offset						; Stores the memory address of the array in the EDX register
+	mov ecx, array_length						; Sets the loop count to the length of the array
+	mov esi, 0									; Sets the starting index to 0
+
+	; Loop that sums the elements of the array
+	;--------------------------------------------------------
 	sum_elements:
-		mov ebx, [edx + esi]
-		add esi, index
+		mov ebx, [edx + esi]					; Stores an element in the EBX register
+		add esi, element_size					; Increments the ESI register to the next element index
 
-		cmp ebx, lowerLimit
-		jl skip_addition
+		; Comparison Checks
+		cmp ebx, lowerLimit						; Compares the value in the EBX register to the lower limit
+		jl skip_addition						; If EBX < lowerLimit -> skips the addition of this particular element
+		cmp ebx, upperLimit						; Compares the value in the EBX register to the upper limit
+		jg skip_addition						; If EBX > upperLimit -> skips the addition of this particular element
 
-		cmp ebx, upperLimit
-		jg skip_addition
+		add eax, ebx							; Adds the value in the EBX register to the total sum in the EAX register			
 
-		add eax, ebx
-
-		skip_addition:
+		skip_addition:							; Code label to skip addition process
+			; Decrements ECX register (loop counter)
+			; Checks if the ECX register is 0
+			; If so -> exit loop, else -> loop sum_elements
 			loop sum_elements
-	ret
+
+	ret											; Returns back to the call statment
 SumArray endp
 end main
